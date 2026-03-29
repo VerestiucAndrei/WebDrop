e31def3fbba7f2b226623cee91570a934860f42f
AndreiVerestiuc <andrei.verestiuc@gmail.com>
2026-03-29 05:38:25 +0300

    Fixed used storage statistic. Started working on multiple file storage

--- public/script.js
+++ public/script.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-import { getDatabase, ref as dbRef, set, onValue } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-import { getStorage, ref as storageRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
-
+import { getDatabase, ref as dbRef, set, onValue, update, increment } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+import { getStorage, ref as storageRef, uploadBytesResumable, getDownloadURL, listAll, deleteObject, getMetadata } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDraLhdj40PioYIGBRhxc1a3s_pz3vAq0E",
@@ -11,6 +10,9 @@ const firebaseConfig = {
     projectId: "webdrop-45d42",
     storageBucket: "webdrop-45d42.firebasestorage.app"
 };
+
+let usedStorageBytes = 0;
+const maxStorageBytes = 5 * 1024 * 1024 * 1024; // 5 GB
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -28,7 +30,7 @@ onAuthStateChanged(auth, (user) => {
 window.manualLogin = function () {
     const email = document.getElementById('emailInput').value;
     const pass = document.getElementById('passInput').value;
-    
+
     signInWithEmailAndPassword(auth, email, pass)
         .then(() => document.getElementById('loginScreen').style.display = 'none')
         .catch(err => alert(err.message));
@@ -56,18 +58,20 @@ function startAppLogic() {
     });
 
     window.uploadFile = function () {
-        // Update the "Total Usage" in the database
-        onValue(dbRef(database, 'stats/totalBytes'), (snapshot) => {
-            const currentTotal = snapshot.val() || 0;
-            const newTotal = currentTotal + file.size;
-            set(dbRef(database, 'stats/totalBytes'), newTotal);
-        }, { onlyOnce: true });
-
         const fileInput = document.getElementById('filePicker');
         const statusText = document.getElementById('status');
         const file = fileInput.files[0];
 
         if (!file) return alert("Pick a file first!");
+
+        // Check if file exceeds remaining storage capacity
+        if (file &&file.size + usedStorageBytes > maxStorageBytes)
+            return alert("Upload blocked: Not enough free space");
+
+        // Update the "Total Usage" in the database
+        update(dbRef(database, 'stats'), {
+            totalBytes: increment(file.size)
+        });
 
         const vaultLocation = storageRef(storage, 'uploads/' + file.name);
         const uploadTask = uploadBytesResumable(vaultLocation, file);
@@ -85,12 +89,45 @@ function startAppLogic() {
                 });
             }
         );
+
     };
 
+
+    window.loadFiles = function () {
+        const listFolderRef = storageRef(storage, 'uploads/');
+        const fileListHTML = document.getElementById('fileList');
+        fileListHTML.innerHTML = "Loading files";
+
+        listAll(listFolderRef).then((res) => {
+            fileListHTML.innerHTML = "";
+
+            if (res.items.length == 0) {
+                fileListHTML.innerHTML = "<li>Vault is empty.</li>";
+                return;
+            }
+
+            res.items.forEach((itemRef) => {
+                const line = document.createElement('line');
+                li.style.marginBottom = "10px";
+                li.style.display = "flex";
+                li.style.justifyContent = "space-between";
+
+                li.innerHTML = `
+                <span>${itemRef.name}</span>
+                <button onclick="deleteFile('${itemRef.name}')" style="background: #ff3b30; padding: 5px 10px;">Delete</button>
+                `;
+                fileListHTML.appendChild(li);
+            });
+        });
+    };
+
+
+    // Update storage statistics
     onValue(dbRef(database, 'stats/totalBytes'), (snapshot) => {
-    const bytes = snapshot.val() || 0;
-    const gb = (bytes / (1024 * 1024 * 1024)).toFixed(2);
-    document.getElementById('usageStat').innerText = gb + " GB";
-    document.getElementById('usageBar').style.width = (gb / 5 * 100) + "%";
-});
+        usedStorageBytes = snapshot.val() || 0;
+        const gb = (bytes / (1024 * 1024 * 1024)).toFixed(4);
+
+        document.getElementById('usageStat').innerText = gb + " GB";
+        document.getElementById('usageBar').style.width = (usedStorageBytes / maxStorageBytes * 100) + "%";
+    });
 }
